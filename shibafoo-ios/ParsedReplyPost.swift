9b3a21b595180c490a0a50f7e66cb13f70490f56
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2016-11-22 00:01:37 +0900

    自分へReplyされたFoo!を表示できるようにした

--- shibafoo-ios/ParsedReplyPost.swift
+++ shibafoo-ios/ParsedReplyPost.swift
@@ -9,28 +9,38 @@
 import UIKit
 
 class ParsedReplyPost: NSObject {
+    var id: Int?
     var content: String?
     var nickname: String?
     var title: String?
     var avatarURL: URL?
-    // ラブされた数
-    // リプライ数
+    var lovesCount: Int?
+    var isLoved: String?
+    var inReplyToUserName: String?
     var createdAt: String?
     
-    init(content: String?, createdAt: String?, avatarURL: URL?, nickname: String?, title: String) {
+    init(id: Int?, content: String?, createdAt: String?, avatarURL: URL?, nickname: String?, title: String, lovesCount: Int?, isLoved: String?, inReplyToUserName: String?) {
         super.init()
+        self.id = id
         self.content = content
         self.nickname = nickname
         self.title = title
+        self.lovesCount = lovesCount
+        self.isLoved = isLoved
+        self.inReplyToUserName = inReplyToUserName
         self.createdAt = createdAt
         self.avatarURL = avatarURL
     }
     
     override init() {
         super.init()
+        self.id = 1
         self.content = ""
         self.nickname = ""
         self.title = ""
+        self.lovesCount = 0
+        self.isLoved = ""
+        self.inReplyToUserName = ""
         self.createdAt = ""
         self.avatarURL = nil
     }
--- shibafoo-ios/ReplyViewController.swift
+++ shibafoo-ios/ReplyViewController.swift
@@ -9,31 +9,31 @@
 import UIKit
 import Alamofire
 import SwiftyJSON
+import Kingfisher
 
 class ReplyViewController: UITableViewController {
-
     var parsedReplyPosts : Array <ParsedReplyPost> = []
     
     @IBAction func handleRefresh(_ sender: AnyObject?) {
-        self.parsedReplyPosts.append(ParsedReplyPost(content: "New row", createdAt: Date().description, avatarURL: defaultAvatarURL, nickname: "new", title: "title"))
+        self.parsedReplyPosts.append(ParsedReplyPost(id: 1, content: "New row", createdAt: Date().description, avatarURL: defaultAvatarURL, nickname: "new", title: "title", lovesCount: 0, isLoved: "false", inReplyToUserName: "nickname"))
         reloadPosts()
         refreshControl!.endRefreshing()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationItem.title = "ReFoo!"
+        self.navigationItem.title = "みんなのログ"
         reloadPosts()
         let refresher = UIRefreshControl()
-        refresher.addTarget(self, action: #selector(ReplyViewController.handleRefresh(_:)), for: UIControlEvents.valueChanged)
+        refresher.addTarget(self, action: #selector(HomeViewController.handleRefresh(_:)), for: UIControlEvents.valueChanged)
         self.refreshControl = refresher
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
@@ -41,58 +41,126 @@ class ReplyViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return parsedReplyPosts.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomReplyPostCell") as? ParsedReplyPostCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomPostCell") as? ParsedReplyPostCell
         let parsedReplyPost = parsedReplyPosts[(indexPath as NSIndexPath).row]
-        cell?.contentLabel.text = parsedReplyPost.content
+        cell?.contentTextView.text = parsedReplyPost.content
         cell?.createdAtLabel.text = parsedReplyPost.createdAt
         cell?.nicknameLabel.text = parsedReplyPost.nickname
         cell?.titleLabel.text = parsedReplyPost.title
+        cell?.replyUserNicknameLabel.text = parsedReplyPost.inReplyToUserName
+        var lovesCountText = "0 Love"
+        if parsedReplyPost.lovesCount?.description != nil {
+            lovesCountText = (parsedReplyPost.lovesCount?.description)! + " Love"
+        }
+        var lovedState: UIControlState = UIControlState.normal
+        if (parsedReplyPost.isLoved != nil) {
+            if parsedReplyPost.isLoved == "true" {
+                cell?.lovesCount.isSelected = true
+                lovedState = UIControlState.selected
+            } else {
+                cell?.lovesCount.isSelected = false
+                lovedState = UIControlState.normal
+            }
+        }
+        cell?.lovesCount.setTitle(lovesCountText, for: lovedState)
         if parsedReplyPost.avatarURL != nil {
-            DispatchQueue.global().async(execute: {() -> Void in
-                    let avatarImage = UIImage(data: try! Data (
-                        contentsOf: parsedReplyPost.avatarURL! as URL))
-                    DispatchQueue.main.async(execute: {
-                            cell?.avatarImageView.image = avatarImage
-                    })
-                }
-            )
-            cell?.avatarImageView.image = UIImage (data: try! Data(contentsOf: parsedReplyPost.avatarURL! as URL))
-       }
+            let url = URL(string: parsedReplyPost.avatarURL!.description)
+            cell?.avatarImageView.kf.setImage(with: url)
+        }
         return cell!
     }
-
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140.0
+    }
+    
     func reloadPosts() {
         let userDefault = UserDefaults.standard
-        let token = userDefault.object(forKey: "authentication_token") as? String
-        let email = userDefault.object(forKey: "email") as? String
-        Alamofire.request(EndpointConst().URL + "api/posts/reply", parameters: ["email": email!, "token": token!])
-            .responseJSON { data in
-              self.parsedReplyPosts.removeAll(keepingCapacity: true)
-                if let jsonData: DataResponse<Any> = data as DataResponse? {
-                let posts = JSON(jsonData.data!)
-                for (_, post) in posts {
-                    let parsedReplyPost = ParsedReplyPost()
-                    parsedReplyPost.content = post["content"].string
-                    let inputDateFormatter = DateFormatter()
-                    inputDateFormatter.locale = Locale(identifier: "ja")
-                    inputDateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
-                    let date: Date? = inputDateFormatter.date(from: post["created_at"].string!)
-                    let outputDateFormatter = DateFormatter()
-                    outputDateFormatter.dateFormat = "yyy/MM/dd HH:mm"
-                    parsedReplyPost.createdAt = outputDateFormatter.string(from: date!)
-                    if let avatarUrl = post["avatar_url"].string {
-                        parsedReplyPost.avatarURL = URL(string: avatarUrl)
+        
+        if let token = userDefault.object(forKey: "authentication_token") as? String {
+            if let email = userDefault.object(forKey: "email") as? String {
+                let parameters: Parameters = ["email": email, "token": token]
+                Alamofire.request(EndpointConst().URL + "api/posts/reply", parameters: parameters).responseJSON { response in
+                    self.parsedReplyPosts.removeAll(keepingCapacity: true)
+                    if let posts = response.result.value {
+                        for post in posts as! [AnyObject] {
+                            let parsedReplyPost = ParsedReplyPost()
+                            parsedReplyPost.content = post["content"] as? String
+                            let inputDateFormatter = DateFormatter()
+                            inputDateFormatter.locale = Locale(identifier: "ja")
+                            inputDateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+                            let date: Date? = inputDateFormatter.date(from: (post["created_at"] as? String)!)
+                            let outputDateFormatter = DateFormatter()
+                            outputDateFormatter.dateFormat = "yyy/MM/dd HH:mm"
+                            parsedReplyPost.createdAt = outputDateFormatter.string(from: date!)
+                            if let avatarUrl = post["avatar_url"] as? String {
+                                parsedReplyPost.avatarURL = URL(string: avatarUrl)
+                            }
+                            parsedReplyPost.nickname = post["nickname"] as? String
+                            parsedReplyPost.title = post["title"] as? String
+                            parsedReplyPost.lovesCount = post["loves_count"] as? Int
+                            parsedReplyPost.isLoved = post["is_loved"] as? String
+                            parsedReplyPost.id = post["id"] as? Int
+                            parsedReplyPost.inReplyToUserName = post["in_reply_to_user_name"] as? String
+                            self.parsedReplyPosts.append(parsedReplyPost)
+                        }
+                        DispatchQueue.main.async(execute: { ()-> Void in self.tableView.reloadData() })
                     }
-                    parsedReplyPost.nickname = post["nickname"].string
-                    parsedReplyPost.title = post["title"].string
-                    self.parsedReplyPosts.append(parsedReplyPost)
                 }
-                DispatchQueue.main.async(execute: { ()-> Void in self.tableView.reloadData() })
+            } else {
+                // tokenがない場合はログイン画面を表示
+                let navigationController = self.storyboard?.instantiateViewController(withIdentifier: "LoginNavigationController") as? UINavigationController
+                self.navigationController?.present(navigationController!, animated: true, completion: nil)
             }
         }
     }
+    
+    @IBAction func loveButtonDidPush(_ sender: UIButton) {
+        let button = sender
+        let view = button.superview!
+        let cell = view.superview as! ParsedReplyPostCell
+        let indexPath = self.tableView.indexPath(for: cell)
+        let parsedReplyPost = self.parsedReplyPosts[(indexPath?.row)!]
+        var lovesCount = 0
+        if parsedReplyPost.lovesCount == nil {
+            lovesCount = 0
+        } else {
+            lovesCount = parsedReplyPost.lovesCount!
+        }
+        let userDefault = UserDefaults.standard
+        let email = userDefault.object(forKey: "email") as? String
+        let token = userDefault.object(forKey: "authentication_token")
+        let params = ["email": email!, "token": token!, "post_id": parsedReplyPost.id!]
+        
+        if sender.isSelected {
+            Alamofire.request(EndpointConst().URL + "api/loves/unlove", method: .delete, parameters: params)
+                .responseJSON { response in
+                    if response.response?.statusCode == 200 {
+                        if let data = response.result.value as? NSDictionary {
+                            lovesCount = (data["loves_count"] as? Int)!
+                            sender.isSelected = !sender.isSelected
+                            let loveText = String(lovesCount) + " Love"
+                            sender.setTitle(loveText, for: UIControlState.normal)
+                        }
+                    }
+            }
+        } else {
+            Alamofire.request(EndpointConst().URL + "api/loves", method: .post, parameters: params)
+                .responseJSON { response in
+                    if response.response?.statusCode == 200 {
+                        if let data = response.result.value as? NSDictionary {
+                            lovesCount = (data["loves_count"] as? Int)!
+                            sender.isSelected = !sender.isSelected
+                            let loveText = String(lovesCount) + " Love"
+                            sender.setTitle(loveText, for: UIControlState.selected)
+                        }
+                    }
+            }
+        }
+    }
+
     
     /*
     // MARK: - Navigation
